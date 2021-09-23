6b056db14b56b89ee3d2e7c0722bf420bab21932
victor-jvp <ing.vj15@gmail.com>
2021-09-22 22:41:36 -0400

    Update MoviesGrid.jsx

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,10 +1,10 @@
-import { useEffect } from 'react';
-import { MovieCard } from './MovieCard'
-import movies from './movies.json'
-import styles from './MoviesGrid.module.css'
+import { useEffect, useState } from "react";
+import { MovieCard } from "./MovieCard";
+import styles from "./MoviesGrid.module.css";
 
 export function MoviesGrid() {
-  
+  // let movies = [];
+  const [movies, setMovies] = useState([])
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/discover/movie`, {
       headers: {
@@ -12,8 +12,12 @@ export function MoviesGrid() {
           "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzUzN2ZmMTlmMzgxZGQ3YjY3ZWVlMWVhOGI4MTY0YSIsInN1YiI6IjVlM2ExNmU1MGMyNzEwMDAxODc1NTI4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nOpZ_nBtA93tbzr6-rxD0760tssAAaSppyjRv9anArs",
         "Content-Type": "application/json;charset=utf-8",
       },
-    });
-  })
+    })
+      .then((result) => result.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  });
 
   return (
     <ul className={styles.moviesGrid}>
