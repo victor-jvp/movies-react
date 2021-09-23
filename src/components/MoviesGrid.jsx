a1aa14b8ac3436c689ebe9b292e184ec21d55b47
victor-jvp <ing.vj15@gmail.com>
2021-09-22 22:20:49 -0400

    added first api to list of movies

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
 import { MovieCard } from './MovieCard'
 import movies from './movies.json'
 import styles from './MoviesGrid.module.css'
 
 export function MoviesGrid() {
   
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/discover/movie`, {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzUzN2ZmMTlmMzgxZGQ3YjY3ZWVlMWVhOGI4MTY0YSIsInN1YiI6IjVlM2ExNmU1MGMyNzEwMDAxODc1NTI4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nOpZ_nBtA93tbzr6-rxD0760tssAAaSppyjRv9anArs",
+        "Content-Type": "application/json;charset=utf-8",
+      },
+    });
+  })
+
   return (
     <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
