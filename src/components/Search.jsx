83bc5d89d79a65dd70a7d7266ba6b9980859c583
victor-jvp <ing.vj15@gmail.com>
2021-09-28 23:25:29 -0400

    implement useDebounce

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,5 @@
 import styles from "./Search.module.css";
 import { FaSearch } from "react-icons/fa";
-import { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import { useQuery } from "../hooks/useQuery";
 
@@ -8,16 +7,10 @@ export default function Search() {
   const query = useQuery();
   const search = query.get("search");
 
-  useEffect(() => {
-    setSearchText(search || "");
-  }, [search]);
-
-  const [searchText, setSearchText] = useState("");
   const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    history.push(`/?search=${searchText}`);
   };
   
   return (
@@ -26,8 +19,11 @@ export default function Search() {
         <input
           className={styles.searchInput}
           type="text"
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
+          value={search}
+          onChange={(e) => {
+            const value = e.target.value;
+            history.push(`/?search=${value}`);
+          }}
         />
         <button className={styles.searchButton} type="submit">
           <FaSearch size={20} />
